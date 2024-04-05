3512af7fd954554fae8e0e4774ca39f55bc6a88b
Fernando Blasco Martín <37153521+FernandoSumun@users.noreply.github.com>
2024-04-05 16:41:41 +0200

    Soporte para ordenar la carta

--- inc/cpt.php
+++ inc/cpt.php
@@ -32,7 +32,7 @@ function custom_post_carta() {
             'capability_type' => 'post',
             'hierarchical' => false,
             /* the next one is important, it tells what's enabled in the post editor */
-            'supports' => array( 'title', 'editor', 'excerpt', 'thumbnail' )
+            'supports' => array( 'title', 'editor', 'excerpt', 'thumbnail', 'page-attributes' )
         ) /* end of options */
     ); /* end of register post type */
     
